10769c71b436423d27d375c66bbb2c147aee8c82
Munir Safi <munir.safi1@gmail.com>
2019-03-24 22:35:37 -0400

    test: capture the event that's being emitted

--- test/fiber_api.js
+++ test/fiber_api.js
@@ -1,4 +1,5 @@
 const SampleContract = artifacts.require("../contracts/SampleContract.sol");
+const Fiber = artifacts.require("../contracts/Fiber.sol");
 
 contract('SampleContract', async (accounts) => {
     
@@ -6,8 +7,19 @@ contract('SampleContract', async (accounts) => {
 
     it("should return a network ID of 0 if it published successfully", async () => {
         const instance = await SampleContract.deployed();
-        const capturedID = await instance.getNet.call();
-        assert.equal(0, capturedID.toNumber(), "The addresses don't match");
+        const networkID = await instance.getNet.call();
+        assert.equal(0, networkID.toNumber(), "The network ID returned is incorrect");
+    });
+
+    it("should emit a Fiber request event", async () => {
+        const instance = await SampleContract.deployed();
+        const fiberContract = new web3.eth.Contract(Fiber.abi, Fiber.address);
+
+        let events = [];
+        fiberContract.events.FiberReq({fromBlock: "latest"}, (err, event) => events.push(event));
+        await instance.testEvent({ from: accounts[0], gas: 450000 });
+
+        assert.equal(events.length, 1, "The Fiber Request event was not emitted");
     });
 
 });
