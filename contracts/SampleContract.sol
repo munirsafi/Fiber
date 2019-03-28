48a9044584a9a27bf20bee49da28d404b79ef65d
Munir Safi <munir.safi1@gmail.com>
2019-03-27 20:41:00 -0400

    feat: oracle server now calls on callback function

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -4,12 +4,18 @@ import "./FiberAPI.sol";
 
 contract SampleContract is runningFiber {
 
+    string public bitcoinPrice;
+
     function testEvent() public returns(bytes32 queryID) {
         queryID = fiber_req("URL", "https://google.com/");
     }
 
     function getNet() public pure returns(int) {
         return runningFiber.networkID;
+    }
+
+    function __callback(bytes32 _fiberID, string memory _response) public {
+        bitcoinPrice = _response;
     }
 
 }
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const Web3 = require('web3'); 
 
 const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
-const fiberAPI = [{ "anonymous": false, "inputs": [ { "indexed": false, "name": "caller", "type": "address" }, { "indexed": false, "name": "calltype", "type": "string" }, { "indexed": false, "name": "req", "type": "string" } ], "name": "FiberReq", "type": "event", "signature": "0x06f77ffe0cb043d6c2ff81fddce2e251023aa7a9e0986d6e71534a011bc5c9b6" }, { "constant": false, "inputs": [ { "name": "_calltype", "type": "string" }, { "name": "_req", "type": "string" } ], "name": "request", "outputs": [ { "name": "queryID", "type": "bytes32" } ], "payable": false, "stateMutability": "nonpayable", "type": "function", "signature": "0x01d511f1" }];
+const fiberABI = [{ "anonymous": false, "inputs": [ { "indexed": false, "name": "caller", "type": "address" }, { "indexed": false, "name": "calltype", "type": "string" }, { "indexed": false, "name": "req", "type": "string" } ], "name": "FiberReq", "type": "event", "signature": "0x06f77ffe0cb043d6c2ff81fddce2e251023aa7a9e0986d6e71534a011bc5c9b6" }, { "constant": false, "inputs": [ { "name": "_calltype", "type": "string" }, { "name": "_req", "type": "string" } ], "name": "request", "outputs": [ { "name": "queryID", "type": "bytes32" } ], "payable": false, "stateMutability": "nonpayable", "type": "function", "signature": "0x01d511f1" }];
+const callbackABI = [{ "constant": false, "inputs": [{ "name": "_fiberID", "type": "bytes32" }, { "name": "_response", "type": "string" } ], "name": "__callback", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }];
+
+
+let accounts = [];
+(async () => {
+    accounts = await web3.eth.getAccounts();
+})();
+
 
 // The address will need to be changed after its initial deployment, but will remain the same after
-const Fiber = new web3.eth.Contract(fiberAPI, '0xa57d0B20BcCcC137779fe6C55d1AD1644f83706D');
+const Fiber = new web3.eth.Contract(fiberABI, '0x7Ed1826B9590d91Fe0A321830498480B3c9202b0');
 Fiber.events.FiberReq({
     fromBlock: 'latest',
-}, (err, event) => {
-    const req = event.returnValues;
-    const res = {
-        requester: req.caller,
-        type: req.calltype,
-        source: req.req
-    };
-    console.log(res);
+}, async (err, event) => {
+    try {
+        const req = event.returnValues;
+        const res = {
+            requester: req.caller,
+            type: req.calltype,
+            source: req.req
+        };
+        const genericContract = await new web3.eth.Contract(callbackABI, res.requester);
+        const randomID = (res.requester + (new Date()).getTime()).substring(0, 31);
+        const response = await genericContract.methods.__callback(web3.utils.fromAscii(randomID), 'test').send({ from: accounts[0], gas: 450000 });
+    } catch(e) {
+        console.log(e);
+    }
 });
